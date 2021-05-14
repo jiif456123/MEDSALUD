2e06157d4cf4c60eb61d6441ccb320430d9891e3
Renzo Reccio <reccio_2001@hotmail.com>
2021-05-13 22:10:05 -0500

    correccion

--- backend/router/network.js
+++ backend/router/network.js
@@ -1,9 +1,9 @@
 const citasController = require('../modules/citas/controller/citas.controller');
-const citasController = require('../modules/citas/controller/citas.controller');
+const farmaciaController = require('../modules/citas/controller/citas.controller');
 
 const rutas = function(app) {
     app.use('/citas', citasController)
-    app.use('/farmacia', citasController)
+    app.use('/farmacia', farmaciaController)
 }
 
 module.exports = rutas;

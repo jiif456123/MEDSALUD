dfa69dde40bb328a39d7df865429ae4f944b9191
Renzo Reccio <reccio_2001@hotmail.com>
2021-06-01 18:15:48 -0500

    avance cus gestionar-caja

--- frontend/src/app/citas/citas.module.ts
+++ frontend/src/app/citas/citas.module.ts
@@ -5,13 +5,18 @@ import { CitasRoutingModule } from './citas-routing.module';
 import { GestionarCitasComponent } from './gestionar-citas/gestionar-citas.component';
 import { GestionarPacienteComponent } from './gestionar-paciente/gestionar-paciente.component';
 import { GestionarCajaComponent } from './gestionar-caja/gestionar-caja.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from "ng2-search-filter";
 
 
 @NgModule({
   declarations: [GestionarCitasComponent, GestionarCajaComponent, GestionarPacienteComponent],
   imports: [
     CommonModule,
-    CitasRoutingModule
+    CitasRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule
   ]
 })
 export class CitasModule { }
---
+++ frontend/src/app/citas/models/motivo.model.ts
@@ -0,0 +1,5 @@
+export interface Motivo {
+    _id: string
+    descripcion: string
+  }
+  

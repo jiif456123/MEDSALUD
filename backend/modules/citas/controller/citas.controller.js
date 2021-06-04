e0ce28ab35868606049acfd1549932412ab1ea95
Stephano <stephanobryan456@hotmail.com>
2021-06-03 19:00:39 -0500

    cus historial clinico

--- backend/modules/citas/controller/citas.controller.js
+++ backend/modules/citas/controller/citas.controller.js
@@ -1,7 +1,7 @@
-
 const router = require('express').Router();
 const pacienteController = require('../paciente/paciente.controller')
+const historiaController = require('../historia/historia.controller')
 
 router.use('/paciente', pacienteController);
-
+router.use('/historia', historiaController);
 module.exports = router;
--- frontend/src/app/citas/citas-routing.module.ts
+++ frontend/src/app/citas/citas-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GestionarCitasComponent } from './gestionar-citas/gestionar-citas.component';
-
+import { GestionarHistoriaComponent } from './gestionar-Historial Clinica/gestionar-historia.component';
 const routes: Routes = [
   {
     path: "gestionar-citas", component: GestionarCitasComponent, data: { title: 'Gestionar Cita' }
+  },
+  {
+    path: "gestionar-historia", component: GestionarHistoriaComponent, data: { title: 'Gestionar Historia' }
   },
 ];
 

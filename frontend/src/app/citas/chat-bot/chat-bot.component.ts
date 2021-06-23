9d0b5c6b52b75956a62551b43c45a0bbdd25d47c
Renzo Reccio <reccio_2001@hotmail.com>
2021-06-22 19:20:23 -0500

    avance cus chatbot

---
+++ frontend/src/app/citas/chat-bot/chat-bot.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-bot',
+  templateUrl: './chat-bot.component.html',
+  styleUrls: ['./chat-bot.component.css']
+})
+export class ChatBotComponent implements OnInit {
+  
+  constructor(
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+  }
+
+}

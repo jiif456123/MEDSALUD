dd60236fdb910d823d1fbb504594db0a512c2bd0
charles53681 <72112858+charles53681@users.noreply.github.com>
2021-06-30 04:34:48 -0500

    z

--- backend/modules/citas/controller/citas.controller.js
+++
@@ -1,7 +0,0 @@
-
-const router = require('express').Router();
-const pacienteController = require('../paciente/paciente.controller')
-
-router.use('/paciente', pacienteController);
-
-module.exports = router;
